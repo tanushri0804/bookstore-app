7ee928dda9df3fb580669b1dfafc96640e2692a3
tanushri0804 <tanushri98371@gmail.com>
2025-01-30 17:54:47 +0530

    bookstore-appp

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {HashRouter} from "react-router-dom"
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <HashRouter>
     <App />
-  </React.StrictMode>
-);
+    </HashRouter>
+  </StrictMode>,
+)
